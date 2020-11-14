7f299c48f2309d65e3745d7ce1842624ab45c478
Zach Lambert <zachalambert@gmail.com>
2020-11-14 16:32:17 +0000

    Leg gait working

--- get_leg_waypoints.m
+++ get_leg_waypoints.m
@@ -1,24 +1,25 @@
-function waypoints = get_leg_waypoints(start_config, goal_config, N, u_max)
+function waypoints = get_leg_waypoints(start_config, mid_config, goal_config, N, T)
     % Returns a trajectory of leg waypoints, where each element
     % specifies time and position
-    % Time is normalised and goes from 0 to u_max
+    % Max time is given as T. Can be normalised time.
     
     % Fit waypoints to cubic function:
-    % x = k_3*t^3 + k_2*T_2 + k_1*T + k_0
-    % (T = u_max)
+    % x = k4*t^4 + k3*t^3 + k2*T_2 + k1*T + k0
     
     waypoints = zeros(N, 4);
-    u = linspace(0, u_max, N);
-    waypoints(:, 4) = u;
+    t = linspace(0, T, N);
+    waypoints(:, 4) = t;
     
     % Copied from get_leg_waypoints_symb
     for i=1:3
         x0 = start_config(i);
+        xMid = mid_config(i);
         xT = goal_config(i);
         k0 = x0;
         k1 = 0;
-        k2 = -(3*(x0 - xT))/u_max^2;
-        k3 = (2*(x0 - xT))/u_max^3;
-        waypoints(:,i) = k3*u.^3 + k2*u.^2 + k1*u + k0;
+        k2 = -(11*x0 - 16*xMid + 5*xT)/T^2;
+        k3 = (2*(9*x0 - 16*xMid + 7*xT))/T^3;
+        k4 = -(8*(x0 - 2*xMid + xT))/T^4;
+        waypoints(:,i) = k4*t.^4 + k3*t.^3 + k2*t.^2 + k1*t + k0;
     end
 end
--- get_leg_waypoints_symb.m
+++ get_leg_waypoints_symb.m
@@ -1,21 +1,25 @@
 % Fit x(t) to:
-% x(t) = k3*t^3 + k2*t_2 + k1*t_1 + k0
+% x(t) = k4*t^4 + k3*t^3 + k2*t_2 + k1*t_1 + k0
 % x(0) = x0
 % x(T) = xT
 % x_dot(0) = 0
-% x_dot(T) = 
-syms k0 k1 k2 k3 T x x_dot t x0 xT
+% x_dot(T) = 0
+% x(T/2) = xMid
+syms k0 k1 k2 k3 k4 T x x_dot t x0 xT xMid
 
-x = k3*t^3 + k2*t^2 + k1*t + k0;
+x = k4*t^4 + k3*t^3 + k2*t^2 + k1*t + k0;
 x_dot = diff(x, t);
 
 eq1 = [subs(x, t, 0) == x0];
 eq2 = [subs(x, t, T) == xT];
 eq3 = [subs(x_dot, t, 0) == 0];
 eq4 = [subs(x_dot, t, T) == 0];
-eqs = [eq1, eq2, eq3, eq4];
-S = solve(eqs, k0, k1, k2, k3);
+eq5 = [subs(x, t, T/2) == xMid];
+
+eqs = [eq1, eq2, eq3, eq4, eq5];
+S = solve(eqs, k0, k1, k2, k3, k4);
 k0 = S.k0
 k1 = S.k1
 k2 = S.k2
 k3 = S.k3
+k4 = S.k4
