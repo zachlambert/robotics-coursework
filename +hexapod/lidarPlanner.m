0ea0f802180bf73e48cdb03b5db3fe47d153abf6
Zach Lambert <zachalambert@gmail.com>
2020-11-25 13:32:27 +0000

    Lidar navigation working, with output figures

--- +hexapod/lidarPlanner.m
+++ +hexapod/lidarPlanner.m
@@ -1,46 +1,59 @@
-classdef lidarPlanner
+classdef lidarPlanner < matlab.System
+    % Public, tunable
     properties
         GoalPose,
-        SensorOffset,
         SensorAngles,
-        MaxRange,
-        Validator,
-        Planner
+        MapRes,
+        MapXLimits,
+        MapYLimits,
     end
     
-    methods
-        function obj = lidarPlanner(goalPose, sensorOffset, sensorAngles, maxRange, mapRes, mapXLimits, mapYLimits)
-            obj.GoalPose = goalPose;
-            obj.SensorOffset = sensorOffset;
-            obj.SensorAngles = sensorAngles;
-            obj.MaxRange = maxRange;
+    properties(DiscreteState)
+        Map,
+        Validator,
+        Planner,
+        PathUpdateCounter
+    end
+    
+    methods(Access = protected)
+        function setupImpl(obj)
             
-            map = binaryOccupancyMap(...
-                [mapXLimits(2)-mapXLimits(1), mapYLimits(2)-mapYLimits(1)],...
-                mapRes...
+        end
+        
+        function waypoints = stepImpl(obj, pose, lidar_range)
+            scan = lidarScan(lidar_range, obj.SensorAngles);
+            insertRay(obj.Map, pose, scan, obj.maxRange);
+               
+            % TODO: Make the following only occur when the
+            % current path is invalidated
+            obj.PathUpdateCounter = obj.PathUpdateCounter + 1;
+            if obj.PathUpdateCounter > 20
+                obj.PathUpdateCounter = 0;
+                obj.Validator.Map = obj.Map;
+                obj.Validator.Map.inflate(1.2/res);
+                obj.Planner.StateValidator = obj.Validator;
+                [refpath, ~] = plan(obj.Planner, pose, obj.GoalPose);
+                waypoints = refpath.States(:,1:2);
+            end
+        end
+        
+        function resetImpl(obj)
+            obj.Map = binaryOccupancyMap(...
+                [obj.MapXLimits(2)-obj.MapXLimits(1), obj.MapYLimits(2)-obj.MapYLimits(1)],...
+                obj.MapRes...
             );
             
             ss = stateSpaceSE2;
             ss.StateBounds = [mapXLimits; mapYLimits; -pi pi];
             obj.Validator = validatorOccupancyMap(ss);
-            obj.Validator.Map = map;
-
-            obj.Planner = plannerRRTStar(ss, validator);
+            
+            obj.Planner = plannerRRTStar(ss, obj.Validator);
             % Only want a short-term plan, so don't do many iterations
             obj.Planner.MaxIterations=1000;
             obj.Planner.MaxNumTreeNodes=100;
             obj.Planner.MaxConnectionDistance = 0.8;
-        end
-        
-        function update(obj, pose, lidar_range)
-            scan = lidarScan(lidar_range, obj.SensorAngles);
-            insertRay(obj.Validator.Map, pose, scan, obj.maxRange);
-        end
-        
-        function waypoints = plan(obj, pose)
-            obj.Planner.StateValidator = obj.Validator;
-            [refpath, ~] = plan(obj.Planner, pose, obj.GoalPose);
-            waypoints = refpath.States(:,1:2);
+            
+            obj.PathUpdateCounter = 0;
         end
     end
 end
